8a4776174082b759c6c6b904979d78b5faac6949
Karunakar <karunrg@gmail.com>
2021-02-11 21:12:10 +0000

    Added  tag

--- main.tf
+++ main.tf
@@ -90,14 +90,10 @@ resource "azurerm_public_ip" "catapp-pip" {
 }
 
 resource "azurerm_virtual_machine" "catapp" {
-  name                = "${var.prefix}-meow"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  vm_size             = var.vm_size
-  tags = {
-    Department = "devops"
-  }
-
+  name                          = "${var.prefix}-meow"
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.myresourcegroup.name
+  vm_size                       = var.vm_size
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
 
@@ -125,7 +121,7 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = { Department = "devops" }
 }
 
 # We're using a little trick here so we can run the provisioner without
